87eeb629c3f81f462ff412879bc3834d792da0e5
romanBeya11 <roman.beya@ocsbstudent.ca>
2017-10-27 20:39:48 -0400

    Second version

---
+++ Unit-3-10.py
@@ -0,0 +1,34 @@
+'''
+Created by Roman Beya
+Created on 27-oct-2017
+Created for ICS3U
+This program calculates a grade point average with input by the user
+'''
+import ui
+
+# GLOBAL variables
+counter = 0
+sum_of_total_marks = 0
+
+def record_marks_touch_up_inside(sender):
+	# Each time this button is pressed, a counter variable will increment by 1
+	global counter
+	counter = counter + 1
+	
+	# This procedure will also record the sum of all marks inputted by the user
+	global sum_of_total_marks
+	sum_of_total_marks = sum_of_total_marks + int(view['enter_marks_textfield'].text)
+	
+	# Display inputted marks
+	view['grade_point_average_label'].text = "Inputted Marks: {}".format(int(view['enter_marks_textfield'].text))
+	
+def calculate_grade_point_average_touch_up_inside(sender):
+	# This procedure will calculate the average using the sum of inputted numbers / counter
+	global counter
+	global sum_of_total_marks
+	
+	grade_point_average = sum_of_total_marks / counter
+	view['grade_point_average_label'].text = "Your grade point average is: {}%".format(grade_point_average)
+	
+view = ui.load_view()
+view.present('sheet')
